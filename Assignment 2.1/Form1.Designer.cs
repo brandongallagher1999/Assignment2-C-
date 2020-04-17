07e865de67abe272065d150083a1dde32402c6e0
Brandon Gallagher <brandonegallagher@gmail.com>
2020-04-16 21:09:40 -0400

    Added the UI and back-end functionality with very few fixes left.

--- Assignment 2.1/Form1.Designer.cs
+++ Assignment 2.1/Form1.Designer.cs
@@ -47,6 +47,8 @@
             this.btn_divide = new System.Windows.Forms.Button();
             this.btn_clear = new System.Windows.Forms.Button();
             this.txt_output = new System.Windows.Forms.RichTextBox();
+            this.btn_leftBracket = new System.Windows.Forms.Button();
+            this.btn_bracketRight = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btn_number1
@@ -60,6 +62,7 @@
             this.btn_number1.TabIndex = 0;
             this.btn_number1.Text = "1";
             this.btn_number1.UseVisualStyleBackColor = false;
+            this.btn_number1.Click += new System.EventHandler(this.btn_number1_Click);
             // 
             // btn_number2
             // 
@@ -72,6 +75,7 @@
             this.btn_number2.TabIndex = 1;
             this.btn_number2.Text = "2";
             this.btn_number2.UseVisualStyleBackColor = false;
+            this.btn_number2.Click += new System.EventHandler(this.btn_number2_Click);
             // 
             // btn_number3
             // 
@@ -84,6 +88,7 @@
             this.btn_number3.TabIndex = 2;
             this.btn_number3.Text = "3";
             this.btn_number3.UseVisualStyleBackColor = false;
+            this.btn_number3.Click += new System.EventHandler(this.btn_number3_Click);
             // 
             // btn_number4
             // 
@@ -96,6 +101,7 @@
             this.btn_number4.TabIndex = 3;
             this.btn_number4.Text = "4";
             this.btn_number4.UseVisualStyleBackColor = false;
+            this.btn_number4.Click += new System.EventHandler(this.btn_number4_Click);
             // 
             // btn_number5
             // 
@@ -108,6 +114,7 @@
             this.btn_number5.TabIndex = 4;
             this.btn_number5.Text = "5";
             this.btn_number5.UseVisualStyleBackColor = false;
+            this.btn_number5.Click += new System.EventHandler(this.btn_number5_Click);
             // 
             // btn_number6
             // 
@@ -120,6 +127,7 @@
             this.btn_number6.TabIndex = 5;
             this.btn_number6.Text = "6";
             this.btn_number6.UseVisualStyleBackColor = false;
+            this.btn_number6.Click += new System.EventHandler(this.btn_number6_Click);
             // 
             // btn_number7
             // 
@@ -132,6 +140,7 @@
             this.btn_number7.TabIndex = 6;
             this.btn_number7.Text = "7";
             this.btn_number7.UseVisualStyleBackColor = false;
+            this.btn_number7.Click += new System.EventHandler(this.btn_number7_Click);
             // 
             // btn_number8
             // 
@@ -144,6 +153,7 @@
             this.btn_number8.TabIndex = 7;
             this.btn_number8.Text = "8";
             this.btn_number8.UseVisualStyleBackColor = false;
+            this.btn_number8.Click += new System.EventHandler(this.btn_number8_Click);
             // 
             // btn_number9
             // 
@@ -156,6 +166,7 @@
             this.btn_number9.TabIndex = 8;
             this.btn_number9.Text = "9";
             this.btn_number9.UseVisualStyleBackColor = false;
+            this.btn_number9.Click += new System.EventHandler(this.btn_number9_Click);
             // 
             // btn_number0
             // 
@@ -168,6 +179,7 @@
             this.btn_number0.TabIndex = 9;
             this.btn_number0.Text = "0";
             this.btn_number0.UseVisualStyleBackColor = false;
+            this.btn_number0.Click += new System.EventHandler(this.btn_number0_Click);
             // 
             // btn_numberPlusMinus
             // 
@@ -204,6 +216,7 @@
             this.btn_equals.TabIndex = 12;
             this.btn_equals.Text = "=";
             this.btn_equals.UseVisualStyleBackColor = false;
+            this.btn_equals.Click += new System.EventHandler(this.btn_equals_Click);
             // 
             // btn_plus
             // 
@@ -216,6 +229,7 @@
             this.btn_plus.TabIndex = 13;
             this.btn_plus.Text = "+";
             this.btn_plus.UseVisualStyleBackColor = false;
+            this.btn_plus.Click += new System.EventHandler(this.btn_plus_Click);
             // 
             // btn_minus
             // 
@@ -228,6 +242,7 @@
             this.btn_minus.TabIndex = 14;
             this.btn_minus.Text = "-";
             this.btn_minus.UseVisualStyleBackColor = false;
+            this.btn_minus.Click += new System.EventHandler(this.btn_minus_Click);
             // 
             // btn_multiply
             // 
@@ -240,6 +255,7 @@
             this.btn_multiply.TabIndex = 15;
             this.btn_multiply.Text = "X";
             this.btn_multiply.UseVisualStyleBackColor = false;
+            this.btn_multiply.Click += new System.EventHandler(this.btn_multiply_Click);
             // 
             // btn_divide
             // 
@@ -252,6 +268,7 @@
             this.btn_divide.TabIndex = 22;
             this.btn_divide.Text = "/";
             this.btn_divide.UseVisualStyleBackColor = false;
+            this.btn_divide.Click += new System.EventHandler(this.btn_divide_Click);
             // 
             // btn_clear
             // 
@@ -264,14 +281,42 @@
             this.btn_clear.TabIndex = 23;
             this.btn_clear.Text = "CE";
             this.btn_clear.UseVisualStyleBackColor = false;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
             // 
             // txt_output
             // 
+            this.txt_output.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txt_output.Location = new System.Drawing.Point(12, 12);
             this.txt_output.Name = "txt_output";
             this.txt_output.Size = new System.Drawing.Size(466, 75);
             this.txt_output.TabIndex = 24;
             this.txt_output.Text = "";
+            // 
+            // btn_leftBracket
+            // 
+            this.btn_leftBracket.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn_leftBracket.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_leftBracket.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_leftBracket.Location = new System.Drawing.Point(130, 103);
+            this.btn_leftBracket.Name = "btn_leftBracket";
+            this.btn_leftBracket.Size = new System.Drawing.Size(112, 51);
+            this.btn_leftBracket.TabIndex = 25;
+            this.btn_leftBracket.Text = "(";
+            this.btn_leftBracket.UseVisualStyleBackColor = false;
+            this.btn_leftBracket.Click += new System.EventHandler(this.btn_leftBracket_Click);
+            // 
+            // btn_bracketRight
+            // 
+            this.btn_bracketRight.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btn_bracketRight.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_bracketRight.ForeColor = System.Drawing.SystemColors.Control;
+            this.btn_bracketRight.Location = new System.Drawing.Point(248, 103);
+            this.btn_bracketRight.Name = "btn_bracketRight";
+            this.btn_bracketRight.Size = new System.Drawing.Size(112, 51);
+            this.btn_bracketRight.TabIndex = 26;
+            this.btn_bracketRight.Text = ")";
+            this.btn_bracketRight.UseVisualStyleBackColor = false;
+            this.btn_bracketRight.Click += new System.EventHandler(this.btn_bracketRight_Click);
             // 
             // Form1
             // 
@@ -279,6 +324,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
             this.ClientSize = new System.Drawing.Size(490, 397);
+            this.Controls.Add(this.btn_bracketRight);
+            this.Controls.Add(this.btn_leftBracket);
             this.Controls.Add(this.txt_output);
             this.Controls.Add(this.btn_clear);
             this.Controls.Add(this.btn_divide);
@@ -326,6 +373,8 @@
         private System.Windows.Forms.Button btn_divide;
         private System.Windows.Forms.Button btn_clear;
         private System.Windows.Forms.RichTextBox txt_output;
+        private System.Windows.Forms.Button btn_leftBracket;
+        private System.Windows.Forms.Button btn_bracketRight;
     }
 }
 
--- Assignment 2.1/Form1.cs
+++ Assignment 2.1/Form1.cs
@@ -13,27 +13,185 @@ namespace Assignment_2._1
     public partial class Form1 : Form
     {
         Calculator calc;
-
+        string output;
 
         public Form1()
         {
             InitializeComponent();
-            calc = new Calculator();
+            calc = new MemCalculator();
+            output = "";
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            btn_clear.Text
+            // empty for now
+        }
+
+        public void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                this.calculate();
+            }
+            else
+            {
+                Console.WriteLine("not the enter key");
+            }
+        }
+
+        private void changeOutput(string character)
+        {
+            txt_output.Text += character;
+        }
+
+        private void clearOutput()
+        {
+            txt_output.Clear();
+        }
+        private void btn_number1_Click(object sender, EventArgs e)
+        {
+            changeOutput("1");
+        }
+
+        private void btn_number2_Click(object sender, EventArgs e)
+        {
+            changeOutput("2");
+        }
+
+        private void btn_number3_Click(object sender, EventArgs e)
+        {
+            changeOutput("3");
+        }
+
+        private void btn_number4_Click(object sender, EventArgs e)
+        {
+            changeOutput("4");
+        }
+
+        private void btn_number5_Click(object sender, EventArgs e)
+        {
+            changeOutput("5");
+        }
+
+        private void btn_number6_Click(object sender, EventArgs e)
+        {
+            changeOutput("6");
+        }
+
+        private void btn_number7_Click(object sender, EventArgs e)
+        {
+            changeOutput("7");
+        }
+
+        private void btn_number8_Click(object sender, EventArgs e)
+        {
+            changeOutput("8");
+        }
+
+        private void btn_number9_Click(object sender, EventArgs e)
+        {
+            changeOutput("9");
+        }
+
+        private void btn_number0_Click(object sender, EventArgs e)
+        {
+            changeOutput("0");
+        }
+
+        private void btn_plus_Click(object sender, EventArgs e)
+        {
+            changeOutput("+");
+        }
+
+        private void btn_minus_Click(object sender, EventArgs e)
+        {
+            changeOutput("-");
+        }
+
+        private void btn_multiply_Click(object sender, EventArgs e)
+        {
+            changeOutput("*");
+        }
+
+        private void btn_divide_Click(object sender, EventArgs e)
+        {
+            changeOutput("/");
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            this.clearOutput();
+        }
+
+        private void btn_equals_Click(object sender, EventArgs e)
+        {
+            this.calculate();
+        }
+
+        private void calculate()
+        {
+            Console.WriteLine("calc called");
+            this.clearOutput();
+            string temp = txt_output.Text;
+            
+            txt_output.Text = temp;
+        }
+
+        private void btn_leftBracket_Click(object sender, EventArgs e)
+        {
+            changeOutput("(");
+        }
+
+        private void btn_bracketRight_Click(object sender, EventArgs e)
+        {
+            changeOutput(")");
         }
     }
 
 
-    public class Calculator
+    public abstract class Calculator
     {
-        decimal output;
         public Calculator()
+        { }
+
+        public abstract string calculate(string equation);
+
+        public bool isValid(string equation)
         {
-            output = 0;
+            return !equation.Contains("/0"); //If anything's being divided by 0
+        }
+        public string clear()
+        {
+            return "";
+        }
+
+
+    }
+
+    public class MemCalculator : Calculator
+    {
+        DataTable dt;
+        public MemCalculator()
+        {
+            this.dt = new DataTable();
+        }
+
+        public string trimEquation(string equation)
+        {
+            return equation.Replace(" ", "");
+        }
+
+        public override string calculate(string equation)
+        {
+            if (this.isValid(this.trimEquation(equation)) == true)
+            {
+                var val = dt.Compute(equation, "");
+                return val.ToString();
+            }
+            else
+            {
+                return "Cannot Divide by Zero";
+            }
         }
     }
 
